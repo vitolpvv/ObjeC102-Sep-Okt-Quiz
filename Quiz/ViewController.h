b4b45e0683c7443bc46d8740387ae936c1704add
Nikolay Shubenkov <n.shubenkov@gmail.com>
2014-09-23 20:44:25 +0400

    Initial Commit

---
+++ Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by n.shubenkov on 23/09/14.
+//  Copyright (c) 2014 n.shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
