a97a878c376e9f19b1901bd9f2530823775c364b
vitolpvv <admin@Mac-Admin.local>
2014-10-01 14:21:54 +0400

    Добавил перемешивание вариантов ответов

--- Quiz/PSRQuestion.m
+++ Quiz/PSRQuestion.m
@@ -16,10 +16,24 @@
     NSAssert(answers.count > 0, @"it is stupid to create queestion without answers %@",@"privet");
     if (self == [super init]){
         _text = [text copy];
-        _answers = [answers copy];
+//        _answers = [answers copy];
+        _answers = [self shuffleAnswers:answers];
         _image = image;
     }
     return self;
+}
+
+- (NSArray *)shuffleAnswers:(NSArray *)answersArray {
+    srand(time(NULL));
+//    srandom(time(NULL));
+    NSMutableArray *answers = [NSMutableArray arrayWithArray:answersArray];
+    NSMutableArray *shuffledAnswers = [NSMutableArray new];
+    while (answers.count > 0) {
+        int randomIndex = rand() % answers.count;
+        [shuffledAnswers addObject:[answers objectAtIndex:randomIndex]];
+        [answers removeObjectAtIndex:randomIndex];
+    }
+    return shuffledAnswers;
 }
 
 @end
